bcaf3d9d67da711f7751a9af550b2c84c178ac94
ILYA <ilya.biryuk2005@icloud.com>
2024-02-18 12:16:09 +0300

    Update to final

--- AssemblerArifmetics/Source.cpp
+++ AssemblerArifmetics/Source.cpp
@@ -18,7 +18,7 @@ int main() {
 		mov pr1,ax
 		// mov pr1, al - length of short are TWO bytes
 	}
-	std::cout << pr1 << '\n';
+	std::cout << "sum unstable:" << pr1 << '\n';
 	// nadegno
 	_asm {
 		mov al,x1
@@ -28,13 +28,13 @@ int main() {
 		cbw
 		add pr1,ax
 	}
-	std::cout << pr1 << '\n';
+	std::cout << "sum:" << pr1 << '\n';
 	_asm {
 		mov al,x1
 		imul y1
 		mov pr1,ax
 	}
-	std::cout << pr1 << '\n';
+	std::cout << "mul:" << pr1 << '\n';
 	std::cout << "enter two in word (-32768)\n";
 	std::cin >> x2 >> y2;
 	_asm {
@@ -43,27 +43,28 @@ int main() {
 		mov pr2,ax
 		mov pr1,dx //why
 	}
-	std::cout << pr2 << " " << pr1 << '\n';
+	std::cout << "mul word:" << pr2 << " " << pr1 << '\n';
 	_asm {
 		mov ax, x2
 		imul y2
 		mov word ptr pr3, ax
 		mov word ptr pr3+2, dx
 	}
-	std::cout << pr3 << '\n';
+	std::cout << "mul stable:" << pr3 << '\n';
 	_asm {
 		mov al,20
 		imul x1
 		mov pr2,ax
 	}
-	std::cout << pr2 << '\n';
+	std::cout << "mul 20:" << pr2 << '\n';
+	x3 = 50;
 	_asm {
 		mov eax,x3
 		// imul ebx,eax,50 or
 		imul ebx,x3,50
 		mov pr3,ebx
 	}
-	std::cout << pr3 << '\n';
+	std::cout << "mul 50 in ebx:" << pr3 << '\n';
 	_asm {
 		mov al,x1
 		cbw
@@ -71,9 +72,49 @@ int main() {
 		cbw
 		mov pr2,ax
 		// mov byte ptr pr1, ah or
-		mov al,ah
-		cbw
-		mov pr1,ax
+		// mov al,ah
+		// cbw
+		// mov pr1,ax - ah clears after cbw
 	}
-	std::cout << pr2 << ' ' << pr1 << '\n';
+	std::cout << "div byte:" << pr2 << '\n';
+	_asm {
+		mov ax, x2
+		cwd
+		idiv y2
+		mov pr2,ax
+	}
+	std::cout << "div short:" << pr2 << '\n';
+	y3 = 50;
+	_asm {
+		mov eax,x3
+		cdq
+		idiv y3
+		mov pr3,eax
+	}
+	std::cout << "div int:" << pr3 << '\n';
+	char a;
+	short b;
+	int c;
+	a = -127;
+	b = -32768;
+	_asm {
+		mov al, a
+		cbw
+		imul b
+		mov word ptr c, ax
+		mov word ptr c+2,dx
+	}
+	std::cout << "mul byte and short:" << c << '\n';
+	_asm {
+		mov ax,b
+		cwde //ax - eax
+		mov ebx,eax
+		mov al,a
+		cbw
+		cwde //ax - eax
+		imul eax,ebx
+		mov c,eax
+	}
+	std::cout << "mul 2 variant:" << c << '\n';
+	return 0;
 }
